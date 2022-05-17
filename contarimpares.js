d5828317f0cdc1992d8e3fe15c9b3b0c4ec3be2d
Gosnejose <nelson_vila@live.com.ar>
2022-05-16 22:16:20 -0300

    first commit

---
+++ contarimpares.js
@@ -0,0 +1,7 @@
+function noParesDeContarImparesHasta(numero){
+    let acumulador = 0
+    for(let i = 0 ; i <= numero; i = i+1) {
+        acumulador = i 
+    }
+   }
+   console.log(function noParesDeContarImparesHasta(5))
---
+++ hizonelson.js
@@ -0,0 +1,6 @@
+function puedePasar(nombre) {
+     let result = (nombre === "Nelson") ? "Hola" : "Chau"
+     return `${result} soy ${nombre}`
+  }
+
+  console.log (puedePasar("Nelson"))
